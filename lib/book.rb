75e6aab3df880c11035687d9443a18d584fd9995
Patrick Carrera <patricklcarrera@gmail.com>
2023-01-31 09:45:41 -0600

    commit tree 988259f78f385733b66be8d504c9c332372e3269

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
 
+class Book
+        attr_accessor:title
+        attr_accessor:author
+        attr_accessor:page_count
+        attr_accessor:genre
+        def initialize(title)
+            @title = title
+            @author = nil
+            @page_count = nil
+            @genre = nil
+        end
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
